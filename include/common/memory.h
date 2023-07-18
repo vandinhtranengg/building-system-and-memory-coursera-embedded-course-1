9d2d836f98772beb3aca0103efd82f143f0322d7
vandinhtranengg <vandinhtranengg@gmail.com>
2023-07-18 17:00:08 +1000

    add some memory manipulation functions's declarations

--- include/common/memory.h
+++ include/common/memory.h
@@ -90,4 +90,101 @@ void set_all(char * ptr, char value, unsigned int size);
  */
 void clear_all(char * ptr, unsigned int size);
 
+
+/*
+ * @brief moves data from source location to destination
+ * Two byte pointers (one source and one destination) are provided with 
+ * a length of bytes for moving the data set. 
+ * Overlap of soucre and destination is handle to avoid data corruption. 
+ *
+ * @param src : Pointer to srouce data array
+ * @param dst : Pointer to destination location
+ * @param length : Length of bytes to move
+ *
+ * @return The pointer to the destination
+ */
+uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length);
+
+
+
+/*
+ * @brief copies data from source location to destination
+ * Two byte pointers (one source and one destination) are provided with
+ * a length of bytes for copying the data set without handle overlap.
+ *
+ *
+ * @param src : Pointer to srouce data array
+ * @param dst : Pointer to destination location
+ * @param length : Length of bytes to move
+ *
+ * @return The pointer to the destination
+ */
+uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length);
+
+
+/*
+ * @brief sets all element of an array to a given value
+ * The pointers to source memory location and assigning value are provided 
+ * with the length of bytes for setting. 
+ *
+ *
+ * @param src : Pointer to srouce data array
+ * @param length: Length of bytes to set
+ * @param value : Given value for setting
+ *
+ * @return The pointer to the source 
+ */
+uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
+
+
+/*
+ * @brief zero out all an array
+ * The pointers to source memory location is provided with the length 
+ * of array's bytes for clearing data.
+ *
+ *
+ * @param src : Pointer to srouce data array
+ * @param length: Length of bytes to set
+ *
+ * @return The pointer to the source
+ */
+uint8_t * my_memzero(uint8_t * src, size_t length);
+
+
+/*
+ * @brief reverse the order of all the bytes of an array
+ * The pointers to memory location of the source array is provided with the length
+ * of array's bytes for reversing. 
+ *
+ *
+ * @param src : Pointer to srouce data array
+ * @param length: Length of bytes to set
+ *
+ * @return The pointer to the source
+ */
+uint8_t * my_reverse(uint8_t * src, size_t length);
+
+
+/*
+ * @brief take number of words to allocate in dynamic memory
+ *
+ *
+ * @param length: number of words
+ *
+ * @return The pointer to the allocated memory
+ */
+int32_t * reserve_words(size_t length);
+
+
+/*
+ * @brief free a dynamic memory allocation
+ *
+ *
+ * @param src : The pointet to the source memory
+ *
+ * @return void
+ */
+void * free_words(int32_t * src);
+
+
 #endif /* __MEMORY_H__ */
