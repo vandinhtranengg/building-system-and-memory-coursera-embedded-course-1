2e354d563188754e6c81041138419bd83e05a205
vandinhtranengg <vandinhtranengg@gmail.com>
2023-07-26 22:48:10 +1000

    for-loop run over ASIC's digits(NULL terminator was taken in the sum-up)- recude the iteration by 1 to fix

--- src/data.c
+++ src/data.c
@@ -8,7 +8,7 @@
 
 #include "data.h"
 #include "memory.h"
-
+#include "platform.h"
 
 uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
 	uint8_t i = 0;
@@ -22,13 +22,36 @@ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
 
 	do {
 		  	// generate digits in reverse order
+		
 		digit = data % base;
 		if(digit < 10)
 			*(ptr + i++) = digit + '0';
 		else // base 16 case: A,B,C,D,E,F
 			*(ptr + i++) = digit + 'A' - 10;
+		
+		switch(base){
+			case BASE_2:
+				data = data >> 1;
+			break;
+			
+			case BASE_8:
+				data = data >> 3;
+			break;
+			
+			case BASE_16:
+				data = data >> 4;
+			break;
+			
+			case BASE_10:
+				data = data/10;
+			break;
 
-	} while ((data /= base) > 0 ); 
+			default:
+			PRINTF("Unknown BASE\n");
+			return 0;
+		}
+
+	} while (data > 0 ); 
 
 
 	if(sign < 0)
@@ -36,7 +59,6 @@ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
 
 	ptr = my_reverse(ptr, (size_t) i);
 	*(ptr + i) = '\0';
-
 	return i + 1;
 }
 
@@ -50,19 +72,21 @@ int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base){
 	if(ch == '-'){
 		i = 2;
 		sign = -1;
-		covert_int = *(ptr + 1)
+		convert_int = (*(ptr + 1) < 'A') ? (*(ptr + 1) - '0') : (*(ptr + 1) - 'A' + 10);
 	}else {
 		i = 1;
 		sign = 1;
-		covert_int = ch;
+		convert_int = (ch < 'A') ? (ch - '0') : (ch - 'A' + 10);
 	}
 	
-	for(i; i < digits; i++){
+	for(;i < digits - 1; i++){
 		ch = *(ptr + i);
-		if(ch < 'A')
+		if(ch < 'A'){
 			convert_int = convert_int*base + (ch - '0');
-		else // BASE_16 case
+		}	
+		else{ // BASE_16 case
 			convert_int = convert_int*base + (ch - 'A' + 10);
+		}
 	}
 	
 	return sign*convert_int;
